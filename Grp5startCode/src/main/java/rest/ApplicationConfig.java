3f766dc30c1be95b0cba0240643a6184261cde52
Foldager94 <cph-cf161@cphbusiness.dk>
2020-11-12 10:22:59 +0100

    responsiv navbar

--- Grp5startCode/src/main/java/rest/ApplicationConfig.java
+++ Grp5startCode/src/main/java/rest/ApplicationConfig.java
@@ -23,7 +23,6 @@ public class ApplicationConfig extends Application {
         resources.add(cors.CorsFilter.class);
         resources.add(errorhandling.API_ExceptionMapper.class);
         resources.add(errorhandling.GenericExceptionMapper.class);
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.AdminResource.class);
         resources.add(rest.DailyFunResource.class);
         resources.add(rest.RenameMeResource.class);
